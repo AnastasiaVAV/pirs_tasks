70c0696b137208b555baaff08e544cab77666884
nastya volkova <derric41@mail.ru>
2026-06-16 19:25:04 +0400

    fix: размер аватара в таблице 50x50px (150px только для страницы просмотра)

--- src/widgets/users-table/UsersTable.tsx
+++ src/widgets/users-table/UsersTable.tsx
@@ -301,7 +301,7 @@ export const UsersTable = () => {
                     <Avatar
                       photoId={user.photo_id}
                       fallback={user.username}
-                      style={{ width: 150, height: 150 }}
+                      style={{ width: 50, height: 50 }}
                     />
                   </TableCell>
                   <TableCell sx={cellSx}>{user.username}</TableCell>
